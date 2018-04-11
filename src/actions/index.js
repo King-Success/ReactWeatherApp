e6a91d7980c82b3600575dbb620f45c768b2fefe
King-Success <okonkwoak@gmail.com>
2018-04-11 22:47:08 +0100

    Implemented the weather reducer and the fetch weather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,8 +5,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`;
+  const url = `${ROOT_URL}&q=${city},ng`;
   const request = Axios.get(url);
+
+  console.log(request);
 
   return {
       type: FETCH_WEATHER, 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
+import weatherReducer from './reducer_weather.js';
 
 
 const rootReducer = combineReducers({
-  
+  weather: weatherReducer
 });
 
 export default rootReducer;
